cde7c7cc6682a10ff39401bd4dbcc1c95b7bc721
AKD47 <van83horn@gmail.com>
2019-07-29 17:18:32 +0300

    add bootstrap

--- src/main.js
+++ src/main.js
@@ -1,10 +1,44 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 import router from './router'
+import axios from 'axios';
+import BootstrapVue from 'bootstrap-vue';
+import ElementUI from 'element-ui';
+import { Loading } from 'element-ui';
+import { Notification } from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
-Vue.config.productionTip = false
+Vue.use(BootstrapVue);
+Vue.use(ElementUI, Loading, Notification);
+
+axios.defaults.baseURL = 'https://dev1amazonbot.dimatech.org';
+axios.defaults.headers.common['Content-Type'] = 'application/json';
+// axios.interceptors.request.use( function (config) {
+//     if (localStorage.getItem('jwt')) {
+//         const token = localStorage.getItem('jwt');
+//         config.headers.common['Authorization'] = `Bearer ${token}`;
+//         // config.headers.authorization = localStorage.getItem("token");
+//     }
+//     return config;
+// });
+// axios.interceptors.response.use(undefined, (error) => {
+//     if (error.request.status ===  0 || error.response.status === 401) {
+//         localStorage.removeItem('jwt');
+//         localStorage.removeItem('username');
+//         localStorage.removeItem('userId');
+//         localStorage.removeItem('apiKey');
+//         router.push({ path: '/spa/login'});
+//     }
+//     return Promise.reject(error);
+// });
+Vue.prototype.$http = axios;
+// set language to EN
+import lang from 'element-ui/lib/locale/lang/en';
+import locale from 'element-ui/lib/locale';
+locale.use(lang);
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   render: function (h) { return h(App) }
-}).$mount('#app')
+}).$mount('#app');
--- src/router.js
+++ src/router.js
@@ -1,27 +1,43 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Vue from 'vue';
+import Router from 'vue-router';
+import Login from './views/Login';
+import Home from './views/Home.vue';
 
-Vue.use(Router)
+Vue.use(Router);
 
-export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: function () { 
-        return import(/* webpackChunkName: "about" */ './views/About.vue')
-      }
+const router = new Router({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes: [
+        {
+            path: '/login',
+            name: 'login',
+            component: Login
+        },
+        {
+            path: '/',
+            name: 'home',
+            component: Home,
+            meta: {
+                requiresAuth: true
+            }
+        }
+    ]
+});
+
+export default router;
+
+router.beforeEach((to, from, next) => {
+    if (to.matched.some(record => record.meta.requiresAuth)) {
+        if (!localStorage.getItem('token')) {
+            next({
+                path: '/login',
+                query: {redirect: to.fullPath}
+            });
+        } else {
+            next();
+        }
+    } else {
+        next();
     }
-  ]
-})
+});
